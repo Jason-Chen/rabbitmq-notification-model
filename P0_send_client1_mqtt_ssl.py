d9678b177cbaba7a006ef7603c657023323b5dd4
Jason YiZhang Chen <a880074@gmail.com>
2014-06-21 15:02:16 +0000

    mqtt ssl connection & password auth

--- P0_send_client1_mqtt_ssl.py
+++ P0_send_client1_mqtt_ssl.py
@@ -1,10 +1,14 @@
 import time
+import ssl
 from datetime import datetime
 import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 client.username_pw_set("test", "test")
-client.connect("127.0.0.1")
+client.tls_set(ca_certs="/etc/rabbitmq/ssl/cacert.pem",
+    certfile="/home/vagrant/mqtt/rabbitC1.cert.pem",
+    keyfile="/home/vagrant/mqtt/rabbitC1.key.pem")
+client.connect("rails-dev-box", 1884)
 
 while(1):
   msg = "Hello client1 " + str(datetime.now())
